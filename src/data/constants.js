4d819f8b2160685d6b725c25b48bd5edc358bac2
Haroon Ahmed <hharoon5166@gmail.com>
2025-04-20 19:40:08 +0400

    workflow added

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/haroon-developerr",
   resume:
-    "https://drive.google.com/file/d/1TRv2-lHyIgRZcUBsmcdy2D2dv3b9eQUt/view?usp=sharing",
+    "https://drive.google.com/file/d/1gWGs8ugcZCe1JvLevm402VifWzbZ1cg3/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/haroon-ahmed-839197318",
   twitter: "https://x.com/HaroonAhmedDev",
 };
